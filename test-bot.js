e96aa2820edb678b7010a2a8f1ee2a9b26e28409
NureFilatovArtem <artem.filatov@nure.ua>
2025-05-12 20:48:08 +0200

    fix: api key

--- test-bot.js
+++ test-bot.js
@@ -1,5 +1,5 @@
 const { Telegraf } = require('telegraf');
-const bot = new Telegraf('8045993727:AAENHDs6N8YJG5fE5auXGJL-hIAOZa6ZGIY'); // <-- Вставь свой токен
+const bot = new Telegraf('TELEGRAM_BOT_TOKEN'); // <-- Вставь свой токен
 
 bot.on('text', (ctx) => {
     console.log('Получено сообщение:', ctx.message.text);
